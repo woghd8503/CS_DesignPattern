ec22e47d76514bade86815579b301c103715f3d1
James <woghd8503@naver.com>
2021-10-07 11:00:00 +0900

    Liskov substitution principle

---
+++ SOLID/SOLID/LiskovSubstitutionPrinciple.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    // using a class example
+    public class Rectangle
+    {
+        //public int Width { get; set; }
+        //public int Height { get; set; }
+
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        //public new  int Width
+        //{
+        //   set { base.Width = base.Height = value; }
+        //}
+
+        //public new int Height
+        //{
+        //   set { base.Width = base.Height = value; }
+
+        public override int Width // nasty side effects
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
+    public class Demo
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            //  should be able to subsitute a base type for a subtype
+            /*spuare*/ Rectangle sq = new Rectangle();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
