361f9d89ba7839da22946a423e7a7b9f59883fbf
James <woghd8503@naver.com>
2022-07-14 23:42:52 +0900

    SOLID

--- SOLID/SOLID/Program.cs
+++ SOLID/SOLID/Program.cs
@@ -12,6 +12,13 @@ namespace SOLID
     {
         private readonly List<string> entries = new List<string>();
 
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add()
+        }
+
         static void Main(string[] args)
         {
             
