94647257d5dd2ed34e7a26cad59570b9652522b5
James <woghd8503@naver.com>
2022-07-20 23:33:38 +0900

    Interface Segregation Principle

---
+++ SOLID/SOLID/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Document
+    {
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+
+    // ok if you need a multifunction machine
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter:IMachine
+    {
+        public void Print(Document d)
+        {
+            // yep
+        }
+
+        public void Fax(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public interface IPriter
+        {
+            void Scan(Document d);
+        }
+
+
+    }
+}
